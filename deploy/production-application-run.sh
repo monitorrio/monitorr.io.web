c415335d5e1ff34d3a1b16eb641728c832da7b59
Konstantin Brovchenko <kbrovchenko@Nerium.com>
2017-10-29 23:44:30 +0300

    Deployment script path updates

--- deploy/production-application-run.sh
+++ deploy/production-application-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-NODE_ENV=production pm2 start --name "production-admin" /home/bitnami/apps/admin.elmahbucket.io/server/server.js
+NODE_ENV=production pm2 start --name "production-admin" /home/bitnami/apps/admin.monitorr.io/server/server.js
 pm2 save
